66b8476a5583e9dac2fc20536f7a4ec2d1935d46
Sandor Laszlo <slo@jedlik.eu>
2024-09-18 14:36:58 +0200

    Layout

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Layout demo app</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        .active {
+            color: red !important;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="mt-4 p-5 bg-primary text-white rounded">
+            <h1>{{ $title ?? "Home" }}</h1>
+        </div>
+        <x-nav></x-nav>
+        
+        <div class="mt-3">{{ $slot }}</div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot name="title">Contacts</x-slot>
+
+    <p>Welcome to the <b>contacts</b> page.</p>
+</x-layout>
